d1ba1bb183fd0768d9081a38df460d03c7e4be31
Kenneth Burchfiel <kburchfiel@gmail.com>
2025-07-19 00:10:19 -0400

    Removed certain files in build folder (including Python scripts and Linux binary) from .gitignore

---
+++ build/mp_visualizations.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Visualization script for multiplayer Type Through the Bible files
+# 
+# By Ken Burchfiel
+# 
+# Released under the MIT license
+# 
+# Note: this script, though developed as a Jupyter Notebook to make development and testing easier, is meant to be called as a Python file within a terminal/command prompt with a multiplayer test results file as its sole argument. Here's an example of what this function call may look like on Linux when the current directory is TTTB's build/ folder:
+# 
+#     python mp_visualizations.py 20250706T220148
+# 
+# Note that only the timestamp component of the multiplayer file(s) is passed to the argument; the function will take care of the rest. (This helps prevent user-submitted strings from getting passed to a system() call, which could potentially cause security issues.
+# 
+# When players complete multiplayer games within Type Through The Bible, this script will then get called via a system() call. However, it can also be run as a standalone file if needed.
+
+# In[1]:
+
+
+import time
+start_time = time.time()
+import os
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+# The following file paths are relative to the build folder.
+mp_results_folder = '../Files/Multiplayer/'
+mp_visualizations_folder = '../Visualizations/Multiplayer/'
+
+
+# Checking whether this script is running within a Jupyter notebook: 
+# 
+# (This will allow us to determine whether to specify our multiplayer filename via argparse (which will only work when the .py version of the file is being rurn) or via a notebook-specific argument.
+
+# In[2]:
+
+
+notebook_exec = False
+try:
+    get_ipython()
+    notebook_exec = True # Script is running within a notebook
+except:
+#     print("get_ipython() failed, so we'll assume that we're running \
+# this script within a .py file.")
+    pass
+
+# notebook_exec
+
+
+
+# Adding code that will allow the caller to specify which multiplayer results file to analyze:
+# 
+# (Note that only the timestamp component of this file should be passed to the argument.)
+# 
+# If you're running this code within a notebook, update the test_results_timestamp value as needed.
+
+# In[3]:
+
+
+if notebook_exec == False:
+    # The following code was based on
+    # https://docs.python.org/3/howto/argparse.html#argparse-tutorial
+    # and # https://docs.python.org/3/library/argparse.html .
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("results_timestamp")
+    args = parser.parse_args()
+    test_results_timestamp = args.results_timestamp
+else:
+    test_results_timestamp = '20250706T220148'
+
+test_results_timestamp
+
+
+# Determining the multiplayer test_results.csv file whose timestamp matches our test_results_timestamp string:
+# 
+# (There *should* only be one such timestamp, but just in case two or more share this timestamp--which is extremely improbable--only one will be retained.)
+
+# In[4]:
+
+
+test_results_file = [file for file in os.listdir(mp_results_folder) if 
+ (test_results_timestamp in file) & ('test_results' in file)][0]
+# Creating a shortened version of this string that doesn't have
+# the 'test_results.csv' component at the end: 
+# (This shortened version will serve as the initial component
+# of our visualization filenames.)
+test_results_name = test_results_file[:-17]
+test_results_file, test_results_name
+
+
+# In[5]:
+
+
+df = pd.read_csv(mp_results_folder+test_results_file) 
+# Replacing the tag columns with their actual meanings:
+df.rename(columns = {'Tag_1':'Round','Tag_2':'Test within round',
+                     'Tag_3':'Game test number'}, inplace = True)
+# Making sure that the results are stored in the order that they were
+# typed:
+df.sort_values('Game test number', inplace = True)
+
+# Adding a column that shows round and test combinations for each race:
+# (This can be used as an x axis value for our WPM-by-race graph.)
+df['Round_Test'] = df['Round'].astype('str') + '_' + df[
+'Test within round'].astype('str')
+# Adding another column that shows how many tests each player has completed
+# so far:
+# (cumcount starts at 0, so we'll need to add 1 to this value for it
+# to work. Also note that the DataFrame doesn't need to be sorted by 
+# player beforehand in order for this code to work correctly.)
+df['Player test number'] = df.groupby(
+    'Player')['WPM'].transform('cumcount') + 1
+# Calculating cumulative WPM values:
+df['Cumulative WPM'] = (df.groupby('Player')['WPM'].transform(
+'cumsum')) / df['Player test number']
+
+df['Best Round_Test WPM'] = df.groupby(
+    'Round_Test')['WPM'].transform('max')
+df['Player had best WPM for this test'] = np.where(
+    df['WPM'] == df['Best Round_Test WPM'], 1, 0)
+
+df.tail()
+
+
+# Creating a melted verison of this DataFrame that stores both cumulative and test-specific WPM values within the same column:
+# 
+# (This will make it easier to produce a graph that uses different line dash types to differentiate between cumulative and test-specific WPM values.)
+
+# In[6]:
+
+
+df_wpm_type_melt = df.melt(id_vars = ['Player', 'Round_Test', 
+'Player test number'],
+value_vars = ['WPM', 'Cumulative WPM'],
+var_name = 'WPM Type',
+value_name = 'Words per minute').rename(
+    columns = {'Words per minute':'WPM'}) # This column couldn't be 
+# initialized as WPM because a column with that name was already present 
+# within df.
+df_wpm_type_melt['WPM Type'] = df_wpm_type_melt['WPM Type'].replace(
+{'WPM':'Test WPM'})
+
+df_wpm_type_melt
+
+
+# In[7]:
+
+
+fig_wpm_by_player = px.line(df_wpm_type_melt, 
+x = 'Player test number', y = 'WPM',
+        color = 'Player', line_dash = 'WPM Type',
+       title = 'WPM By Player and Test',
+        color_discrete_sequence = px.colors.qualitative.Alphabet)
+fig_wpm_by_player.write_html(
+    f'{mp_visualizations_folder}{test_results_name}_Mean_WPM_By_\
+Player_And_Test.html',
+    include_plotlyjs = 'cdn')
+# Note: 'Alphabet' is used here so that up to 26 distinct colors can be
+# shown within the chart (which will prove useful for multiplayer rounds
+# with larger player counts).
+# fig_wpm_by_player
+
+
+# Calculating mean WPMs by player and round as well as overall WPMs:
+
+# In[8]:
+
+
+df_mean_wpm_by_player_and_round = df.pivot_table(
+index = ['Player', 'Round'], values = 'WPM', 
+               aggfunc = 'mean').reset_index()
+
+# Adding overall WPMs for each player to the bottom of this DataFrame:
+
+df_mean_wpm_by_player = df.pivot_table(
+index = 'Player', values = 'WPM', 
+               aggfunc = 'mean').reset_index()
+df_mean_wpm_by_player['Round'] = 'Overall'
+
+df_mean_wpm_by_player_and_round = pd.concat([
+    df_mean_wpm_by_player_and_round, df_mean_wpm_by_player]).reset_index(
+    drop=True)
+# Ensuring that all Round values are strings: (I found that, without
+# this update, the Overall rows would not appear within the figure
+# that we're about to create.)
+df_mean_wpm_by_player_and_round['Round'] = (
+    df_mean_wpm_by_player_and_round['Round'].astype('str'))
+df_mean_wpm_by_player_and_round
+
+
+# In[9]:
+
+
+fig_mean_wpm_by_player_and_round = px.bar(
+    df_mean_wpm_by_player_and_round, x = 'Round', 
+       y = 'WPM', color = 'Player', barmode = 'group',
+    color_discrete_sequence = px.colors.qualitative.Alphabet,
+    title = 'Mean WPM by Player and Round',
+      text_auto = '.0f')
+fig_mean_wpm_by_player_and_round.write_html(
+    f'{mp_visualizations_folder}{test_results_name}_Mean_WPM_\
+By_Player_And_Round.html',
+    include_plotlyjs = 'cdn')
+# fig_mean_wpm_by_player_and_round
+
+
+# In[10]:
+
+
+df_wins = df.pivot_table(index = 'Player', values = 'Player had best \
+WPM for this test', aggfunc = 'sum').reset_index()
+df_wins.sort_values('Player had best WPM for this test', ascending = False,
+                   inplace = True)
+df_wins
+
+
+# In[11]:
+
+
+fig_wins = px.bar(df_wins, x = 'Player', 
+       y = 'Player had best WPM for this test',
+      title = 'Number of Tests in Which Each Player \
+Had the Highest WPM', text_auto = '.0f',
+                 color = 'Player', 
+color_discrete_sequence=px.colors.qualitative.Alphabet).update_layout(
+yaxis_title = 'Wins')
+fig_wins.write_html(f'{mp_visualizations_folder}{test_results_name}_\
+wins_by_player.html', include_plotlyjs = 'cdn')
+# fig_wins
+
+
+# In[12]:
+
+
+df_highest_wpm = df.pivot_table(index = 'Player', values = 'WPM', 
+                                aggfunc = 'max').reset_index()
+df_highest_wpm.sort_values('WPM', ascending = False, inplace = True)
+df_highest_wpm
+
+
+# In[13]:
+
+
+fig_highest_wpm = px.bar(df_highest_wpm, x = 'Player', 
+       y = 'WPM',
+      title = 'Highest WPM by Player', text_auto = '.3f',
+                 color = 'Player', 
+                 color_discrete_sequence = 
+px.colors.qualitative.Alphabet)
+fig_highest_wpm.write_html(
+    f'{mp_visualizations_folder}{test_results_name}_\
+highest_wpm_by_player.html', include_plotlyjs = 'cdn')
+# fig_highest_wpm
+
+
+# In[14]:
+
+
+end_time = time.time()
+run_time = end_time - start_time
+print(f"Finished calculating and visualizing multiplayer stats in \
+{round(run_time, 3)} seconds.")
+
---
+++ build/sp_visualizations.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Visualization scripts for single-player Type Through the Bible files
+# 
+# (Still very much a work in progress--many more analyses to come, including word-level ones and accuracy-focused graphs!)
+# 
+# By Ken Burchfiel
+# 
+# Released under the MIT License
+
+# In[1]:
+
+
+import time
+start_time = time.time()
+import pandas as pd # Polars could also be used in place of Pandas to 
+# speed up some import/analysis processes--but, if there ends up being
+# a need for faster data transformation code, I would rather do it in 
+# C++ (as a means of building up my practice with that language.)
+pd.set_option('display.max_columns', 1000)
+import plotly.express as px
+import numpy as np
+sp_visualizations_folder = '../Visualizations/Single_Player/'
+
+
+# ## Analyzing test result file:
+
+# In[2]:
+
+
+df_tr = pd.read_csv('../Files/test_results.csv') # tr = 'test results'
+
+#Converting start/end timestamps to DateTime values:
+for col in ['Local_Test_Start_Time', 'Local_Test_End_Time']:
+    df_tr[col] = pd.to_datetime(df_tr[col])
+# Ensuring the tests are being displayed in chronological order:
+# (This will help ensure that the 'Test_ID' values that we're about
+# to create are accurate)
+df_tr = df_tr.sort_values(
+    'Local_Test_Start_Time').reset_index(drop=True).copy()
+
+df_tr['Test number'] = df_tr.index + 1
+
+
+df_tr
+
+
+# Calculating various timing statistics that will prove useful for endurance-related analyses:
+
+# In[3]:
+
+
+for time_type in ['Start', 'End']:
+    df_tr[f'{time_type} Date'] = df_tr[
+        f'Local_Test_{time_type}_Time'].dt.date
+    df_tr[f'{time_type} Hour'] = df_tr[
+        f'Local_Test_{time_type}_Time'].dt.hour
+    df_tr[f'{time_type} Minute'] = df_tr[
+        f'Local_Test_{time_type}_Time'].dt.minute
+    df_tr[f'{time_type} 30-Minute Block'] = np.where(
+        df_tr[f'{time_type} Minute'] >= 30, 2, 1)
+    # Using floor division to determine the 15- and 10-minute blocks
+    # into which each test falls:
+    df_tr[f'{time_type} 15-Minute Block'] = df_tr[
+        f'{time_type} Minute'] // 15 + 1
+    df_tr[f'{time_type} 10-Minute Block'] = df_tr[
+        f'{time_type} Minute'] // 10 + 1
+
+# Creating columns that will store unique starting hours and
+# 30/15/10-minute blocks:
+df_tr['Unique Hour'] = df_tr['Start Date'].astype(
+    'str') + '_' + df_tr['Start Hour'].astype('str')
+for block in ['30', '15', '10']:
+    df_tr[f'Unique {block}-Minute Block'] = df_tr[
+        'Unique Hour'] + '_' + df_tr[
+        f'Start {block}-Minute Block'].astype('str')
+
+
+df_tr.head(5)
+
+
+# ## Determining how many characters the player typed after each *rolling* hour, 30-minute period, 15-minute period, and 10-minute period:
+# 
+# (This information will be helpful for calculating endurance-based statistics.)
+
+# In[4]:
+
+
+col_seconds_pair_list = [['Characters Typed in Next Hour', 3600],
+                         ['Characters Typed in Next 30 Minutes', 1800],
+                         ['Characters Typed in Next 15 Minutes', 900],
+                         ['Characters Typed in Next 10 Minutes', 600]]
+
+
+# Note: the following code will likely take a considerable amount of time to run once users have completed a significant number of tests (e.g. 1000+). Therefore, it would be ideal to eventually replace it with C++-based code *or* a more sophisticated Python-based approach. (I tried out the 'numba' engine setting within the df.apply() function, but it failed to work correctly.)
+# 
+# In the meantime, I've commented out this code so that it won't cause performance issues going forward.
+
+# In[5]:
+
+
+# df_tr_condensed = df_tr[['Unix_Test_Start_Time', 
+# 'Unix_Test_End_Time', 'Characters', 'Test number']].copy()
+# I thought this DataFrame might be more compatible with certain
+# alternative engine options, but unfortunately, that wasn't the case.
+
+
+# In[6]:
+
+
+# for col_seconds_pair in col_seconds_pair_list:
+
+#     df_tr[col_seconds_pair[0]] = df_tr.apply(
+#         lambda x: df_tr[(df_tr[
+#             'Unix_Test_Start_Time'] >= x['Unix_Test_Start_Time']) & (
+#             df_tr['Unix_Test_End_Time'] 
+#         < (x['Unix_Test_Start_Time'] + col_seconds_pair[1]))][
+#             'Characters'].sum(), axis = 1)
+# df_tr
+
+
+# Note: I thought the following approach might actually be faster than the above option, as it only requires a single loop through the whole DataFrame. However, I found it to take a bit longer than the previous method.
+
+# In[7]:
+
+
+# for col in ['characters_typed_in_next_hour',
+#             'characters_typed_in_next_30_minutes',
+#             'characters_typed_in_next_15_minutes',
+#             'characters_typed_in_next_10_minutes']:
+#     df_tr[col] = 0
+# for i in range(len(df_tr)):
+#     start_time = df_tr.iloc[i]['Unix_Test_Start_Time'].astype(
+#         'int64')
+
+#     df_tr.iloc[i, df_tr.columns.get_loc(
+#         'characters_typed_in_next_hour')] = df_tr[(
+#         df_tr[
+#         'Unix_Test_Start_Time'] >= start_time) & (df_tr[
+#             'Unix_Test_End_Time'] 
+#         < (start_time + 3600))]['Characters'].sum()
+
+#     df_tr.iloc[i, df_tr.columns.get_loc(
+#         'characters_typed_in_next_30_minutes')] = df_tr[(
+#         df_tr[
+#         'Unix_Test_Start_Time'] >= start_time) & (df_tr[
+#             'Unix_Test_End_Time'] 
+#         < (start_time + 1800))]['Characters'].sum()
+
+#     df_tr.iloc[i, df_tr.columns.get_loc(
+#             'characters_typed_in_next_15_minutes')] = df_tr[(
+#             df_tr[
+#             'Unix_Test_Start_Time'] >= start_time) & (df_tr[
+#                 'Unix_Test_End_Time'] 
+#             < (start_time + 900))]['Characters'].sum()
+
+#     df_tr.iloc[i, df_tr.columns.get_loc(
+#             'characters_typed_in_next_10_minutes')] = df_tr[(
+#             df_tr[
+#             'Unix_Test_Start_Time'] >= start_time) & (df_tr[
+#                 'Unix_Test_End_Time'] 
+#             < (start_time + 600))]['Characters'].sum()
+
+
+
+# In[8]:
+
+
+df_tr
+
+
+# ### WPM results by test:
+
+# In[9]:
+
+
+fig_wpm_by_test = px.line(
+    df_tr, x = 'Test number', y = 'WPM',
+title = 'WPM by Test Number',)
+fig_wpm_by_test.write_html(f'{sp_visualizations_folder}WPM_by_race.html',
+                           include_plotlyjs = 'cdn')
+
+
+# ### Average WPM by Tag 1 values:
+
+# In[10]:
+
+
+df_wpm_by_tag_1 = df_tr.pivot_table(index = 'Tag_1', values = 'WPM',
+                                 aggfunc = 'mean').reset_index()
+df_wpm_by_tag_1
+
+
+# In[11]:
+
+
+fig_wpm_by_tag_1 = px.bar(df_wpm_by_tag_1, x = 'Tag_1', y = 'WPM',
+                         title = 'Mean WPM by Tag_1 value')
+fig_wpm_by_tag_1.write_html(f'{sp_visualizations_folder}/Mean_WPM_\
+by_Tag_1.html',
+                            include_plotlyjs = 'cdn')
+
+
+# ## Creating endurance-related charts:
+
+# ## Visualizing *rolling* endurance statistics:
+# 
+# (I commented out the following visualization code because it relies on a very inefficient set of code that I have also commented out.)
+
+# In[12]:
+
+
+# for col in [pair[0] for pair in col_seconds_pair_list]:
+
+#     df_endurance = df_tr.sort_values(col, ascending = False).copy(
+#     ).reset_index(drop=True).head(50)
+#     df_endurance['Rank'] = (df_endurance.index + 1)
+
+#     fig_endurance = px.bar(
+#     df_endurance, x = 'Rank', 
+#     y = col,
+#     title = 'Most ' + col,
+#     hover_data = ['Test number', 'Local_Test_Start_Time'])
+
+#     fig_endurance.write_html('Single_Player/Endurance_Top_50_rolling_'+col.replace(
+#         ' ', '_')+'.html', 
+#     include_plotlyjs = 'cdn')
+
+
+# ## Visualizing clock-based endurance statistics:
+
+# In[13]:
+
+
+for time_category in ['Hour', '30-Minute Block', '15-Minute Block',
+                      '10-Minute Block']:
+    # The following code helps confirm that the following query() statement
+    # is correctly filtering out tests whose starting and ending 
+    # time categories don't match.
+    # print(len(df_tr.query(f"`Start {time_category}` == `End {time_category}`")))
+    df_endurance = df_tr.query(f"`Start {time_category}` == `End {time_category}`").pivot_table(
+        index = f'Unique {time_category}', values = 'Characters', 
+    aggfunc = 'sum').reset_index().sort_values(
+    'Characters', ascending = False).reset_index(drop=True).head(50)
+    df_endurance['Rank'] = (df_endurance.index + 1)
+
+    fig_endurance = px.bar(
+    df_endurance, x = f'Unique {time_category}', 
+    y = 'Characters',
+    title = 'Most Characters Typed By ' + time_category,
+    hover_data = 'Rank')
+
+    fig_endurance.write_html(
+    f'{sp_visualizations_folder}Endurance_Top_50_\
+Clock_'+time_category.replace(' ', '_')+'.html', 
+    include_plotlyjs = 'cdn')
+
+
+# Graphing keypresses by date (in both chronological and ranked order):
+
+# In[14]:
+
+
+df_top_dates_by_keypresses = df_tr.query(
+    "`Start Date` == `End Date`").pivot_table(
+    index = 'Start Date', values = 'Characters', 
+    aggfunc = 'sum').reset_index().sort_values(
+    'Characters', ascending = False).reset_index(drop=True)
+df_top_dates_by_keypresses['Rank'] = df_top_dates_by_keypresses.index + 1
+# Plotly will automatically arrange these dates in chronological order 
+# despite our having sorted the source DataFrame by characters.
+fig_keypresses_by_date = px.bar(df_top_dates_by_keypresses,
+                                     x = 'Start Date', y = 'Characters',
+                              title = 'Characters typed by date',
+                               hover_data = ['Rank'])
+fig_keypresses_by_date.write_html(f'{sp_visualizations_folder}Keypresses\
+_Typed_by_Date.html', include_plotlyjs='cdn')
+
+
+# In[15]:
+
+
+fig_top_dates_by_keypresses = px.bar(
+    df_top_dates_by_keypresses.head(50),
+       x = 'Start Date', y = 'Characters',
+       title = 'Dates with the most characters typed', 
+       hover_data = ['Rank']).update_layout(
+    xaxis_type = 'category')
+fig_top_dates_by_keypresses.write_html(
+    f'{sp_visualizations_folder}/Top_Dates_by_Keypresses.html', 
+    include_plotlyjs='cdn')
+
+
+# In[16]:
+
+
+end_time = time.time()
+run_time = end_time - start_time
+print(f"Finished calculating and visualizing single-player stats in \
+{round(run_time, 3)} seconds.")
+
